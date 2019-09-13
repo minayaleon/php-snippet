84e117b371e4b6a7b1c4b0a83f902d9faa335d3e
Juan Minaya <minayaleon@gmail.com>
2019-09-13 09:46:46 -0500

    Added new characters

--- windows_1252.php
+++ windows_1252.php
@@ -1,6 +1,7 @@
 <?php 
 
 // https://ourcodeworld.com/articles/read/772/how-to-write-a-text-file-with-ansi-encoding-western-windows-1252-in-php
+// https://www.fileformat.info/info/unicode/char/0192/charset_support.htm
 
 function createWindow1252File() {
     // Windows-1252
@@ -45,11 +46,33 @@ function sanitizeCP1252_v1($input) {
 }
 
 function sanitizeWindow1252($input) {
+    $window1252 = [
+        "\x{80}" => "€",
+        "\x{83}" => "ƒ",
+        "\x{84}" => "„",
+        "\x{86}" => "†",
+        "\x{87}" => "‡",
+        "\x{88}" => "ˆ",
+        "\x{89}" => "‰",
+        "\x{91}|\x{92}" => "'",
+        "\x{95}" => "•",
+        "\x{96}" => "–",
+        "\x{97}" => "—",
+        "\x{98}" => "˜",
+        "\x{99}" => "™",
+    ];
+
     $regexEncoding = \mb_regex_encoding();
     $detectEncoding = \mb_detect_encoding($input, ['UTF-8', 'Windows-1252', 'ISO-8859-1'], true);
     if (in_array($detectEncoding, ['Windows-1252', 'ISO-8859-1'])) {
         \mb_regex_encoding('ISO-8859-1');
-        $output  = \mb_ereg_replace("\x{91}|\x{92}", "'", $input);
+        foreach ($window1252 as $pattern => $replacement) {
+            $output = \mb_ereg_replace($pattern, $replacement, $input);
+            if (!$output) {
+                break;
+            }
+            $input = $output;
+        }
         \mb_regex_encoding($regexEncoding);
         if ($output) {
             return $output;
