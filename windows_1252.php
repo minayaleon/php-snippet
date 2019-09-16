d29be8bfd2d30474023a095d1249ec86361d7059
Juan Minaya <minayaleon@gmail.com>
2019-09-16 14:51:02 -0500

    Added support to right and left double quotation mark

--- windows_1252.php
+++ windows_1252.php
@@ -2,23 +2,42 @@
 
 // https://ourcodeworld.com/articles/read/772/how-to-write-a-text-file-with-ansi-encoding-western-windows-1252-in-php
 // https://www.fileformat.info/info/unicode/char/0192/charset_support.htm
+// https://www.i18nqa.com/debug/table-iso8859-1-vs-windows-1252.html
+// https://dev.to/morinoko/double-quotes-vs-single-quotes-in-php-2e5n
 
-function createWindow1252File() {
+function forceUTF8Encoding(string $input) {
+    $output = \iconv(
+        \mb_detect_encoding($input, ['UTF-8', 'Windows-1252', 'ISO-8859-1'], true),
+        'UTF8//TRANSLIT',
+        $input
+    );
+
+    if ($output === false) {
+        echo 'worn';
+        //throw new InvalidCharacterException();
+    } else {
+        return $input;
+    }
+}
+
+function createWindow1252File($onlyString = false) {
     // Windows-1252
-    //$data = "‘’€†€";
+    $data = "‘’€†€“”";
 
     // ISO-8859-1
-    $data = "‘’€†€Q_1";
-    $string_encoded = iconv(mb_detect_encoding($data), 'Windows-1252//TRANSLIT', $data );
+    //$data = "‘’€†€Q_1“”";
+    $stringEncoded = iconv(mb_detect_encoding($data), 'Windows-1252//TRANSLIT', $data);
 
-    $fileId = uniqid();
-    $filename = 'temp/' . 'cp1252_' . $fileId . '.txt';
-
-    $file = fopen( $filename, "w+");
-    fwrite($file, $string_encoded);
-    fclose($file);
-
-    return $filename;
+    if ($onlyString) {
+        return $stringEncoded;
+    } else {
+        $fileId = uniqid();
+        $filename = 'temp/' . 'cp1252_' . $fileId . '.txt';
+        $file = fopen( $filename, "w+");
+        fwrite($file, $stringEncoded);
+        fclose($file);
+        return $filename;
+    }
 }
 
 function readWindow1252File($win1252File){
@@ -55,6 +74,7 @@ function sanitizeWindow1252($input) {
         "\x{88}" => "ˆ",
         "\x{89}" => "‰",
         "\x{91}|\x{92}" => "'",
+        "\x{93}|\x{94}" => '"',
         "\x{95}" => "•",
         "\x{96}" => "–",
         "\x{97}" => "—",
@@ -64,11 +84,12 @@ function sanitizeWindow1252($input) {
 
     $regexEncoding = \mb_regex_encoding();
     $detectEncoding = \mb_detect_encoding($input, ['UTF-8', 'Windows-1252', 'ISO-8859-1'], true);
+    print $detectEncoding . PHP_EOL;
     if (in_array($detectEncoding, ['Windows-1252', 'ISO-8859-1'])) {
         \mb_regex_encoding('ISO-8859-1');
         foreach ($window1252 as $pattern => $replacement) {
             $output = \mb_ereg_replace($pattern, $replacement, $input);
-            if (!$output) {
+            if ($output === false) {
                 break;
             }
             $input = $output;
@@ -82,11 +103,23 @@ function sanitizeWindow1252($input) {
     }
 }
 
-$win1252File = createWindow1252File();
-echo $win1252File . PHP_EOL;
+$onlyString = true;
 
-$win1252 = readWindow1252File($win1252File);
-print $win1252 . PHP_EOL;
+if ($onlyString) {
+    print 'A' . PHP_EOL;
+    $win1252 = createWindow1252File($onlyString);
+    print $win1252 . PHP_EOL;
+} else {
+    $win1252File = createWindow1252File($onlyString);
+    echo $win1252File . PHP_EOL;
+
+    $win1252 = readWindow1252File($win1252File);
+    print $win1252 . PHP_EOL;
+}
 
 $win1252 = sanitizeWindow1252($win1252);
+//$win1252 = forceUTF8Encoding($win1252);
+$detectEncoding = \mb_detect_encoding($win1252, ['UTF-8', 'Windows-1252', 'ISO-8859-1'], true);
+print $detectEncoding  . PHP_EOL;
+
 print $win1252  . PHP_EOL;
